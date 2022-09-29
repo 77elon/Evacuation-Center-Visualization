5c681117c752f9a3585d59effcb502b71951e6a8
Uuu9 <87708661+GUuu9@users.noreply.github.com>
2022-09-30 00:32:36 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Linking } from 'react-native';
+import { FAB, Portal, Provider, Appbar } from 'react-native-paper';
 
-export default function App() {
+const MyComponent = () => {
+
+  // 하단 FAB
+  const [state, setState] = React.useState({ open: false });
+  const onStateChange = ({ open }) => setState({ open });
+  const { open } = state;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    
+    <Provider>
+      <Appbar.Header>
+       <Appbar.Content title="내 주변 대피소 확인" />
+      </Appbar.Header>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      <Portal>
+        <FAB.Group
+          open={open}
+          icon={open ? 'minus' : 'plus'}
+          actions={[
+            {
+              icon: 'cloud',
+              label: '기상청',
+              onPress: () => Linking.openURL('https://www.weather.go.kr/w/index.do')
+            },
+            {
+              icon: 'target',
+              label: '내 위치 보기',
+              onPress: () => console.log('move my position'),
+            },
+          ]}
+          onStateChange={onStateChange}
+          onPress={() => {
+            if (open) {
+              // do something if the speed dial is open
+            }
+          }}
+        />
+      </Portal>
+    </Provider>
+  );
+};
+
+export default MyComponent;
